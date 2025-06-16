a98d33d88bd59e4075f16b00b478bb60a619fa51
Добровольский <kdobrovolsky02@gmail.com>
2025-06-16 10:42:56 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+
+export function App() {
+  const [count, setCount] = useState(0);
+  const [maxValue, setMaxValue] = useState(10);
+  const [startValue, setStartValue] = useState(0);
+
+  return(
+    <div>
+      
+    </div>
+  )
+
+}
